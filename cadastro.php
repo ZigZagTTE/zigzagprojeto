4dbe33b1c9461b61d403a6632fba23b40ea756b5
Selau <168492821+joaopedroselau@users.noreply.github.com>
2025-05-09 21:39:27 -0300

    Código cadastro.php comentado

--- cadastro.php
+++ cadastro.php
@@ -48,26 +48,41 @@
 <body>
     <?php
 
+        //# Resumo geral
+        // Esta página é dividida em três etapas, cadastro 0, 1 e 2.
+        // A variável $_POST 'cadastroStatus' define a etapa atual, essa variável é passada 
+        // de etapa a etapa dentro de um input escondido (hidden) no final do formulário.
+        // - A etapa 0 recolhe as informações de login da conta;
+        // - a etapa 1 insere os dados na tabela e recolhe as informações pessoais;
+        // - a etapa 2 atualiza a tabela com estes dados e então redireciona o usuário para index.html.
+
+        //# A fazer:
+        // - definir mensagens de erro mySQL (se forem usadas);
+        // - testar a página?
+        // - implementar php no login;
+        // - implementar verificação e configuração de cookies no cadatro, login e possivelmente em todas páginas.
+
         require_once("conecta.php");
 
-        if (!isset($_GET['erro']))
-        {
-            $_GET['erro'] = "0";
-        }
-        if(isset($_POST['txtPagStatus']))
-        {
-            $pagStatus = $_POST['txtPagStatus'];
-        }
-        else
-        {
-            $pagStatus = "cadastro_0";
+        if (!isset($_GET['status']))    // Define a variável $_GET 'status' como 0 caso ela seja nula.
+        {                               // Variáveis $_GET são armazenadas como texto no link. (http://exemplo.com?variavel=exemplo)
+            $_GET['status'] = "0";
         }
 
-        if ($pagStatus == "cadastro_0")
-        {
+        if(isset($_POST['cadastroStatus']))     // Obtem a variável $_POST 'cadastroStatus' caso ela não seja nula
+        {                                       // e define $cadStatus com seu valor.    
+            $cadStatus = $_POST['cadastroStatus'];
+        }
+        else                // Define a variável $_POST 'cadastroStatus' como cadastro_0
+        {                   // caso ela seja nula.
+            $cadStatus = "cadastro_0";
+        }
+
+        if ($cadStatus == "cadastro_0") //# Etapa 0 => caso $cadStatus = "cadastro_0"
+        {                               // - Contém apenas o primeiro formulário.
 
     ?>
-    <form class="popup" method="POST" action="cadastro.php">
+    <form class="popup" method="POST" action="cadastro.php"> <!-- action define que a página será chamada por si mesma. -->
 
         <div class="tit_log">Crie sua conta</div> <!-- ## Formulário das informações de Login -->
         <div class="insc_cad">
@@ -81,8 +96,8 @@
             
             <p class="desc_log">*Crie uma senha com no mínimo 8 caracteres, havendo uma letra maíuscula, uma minúscula e um número. </p><br>
             <?php 
-                if ($_GET['erro'] == "!senha")
-                {
+                if ($_GET['status'] == "erro!senhaDif") // O erro aparece caso a variável $_GET 'status'
+                {                                       // seja "erro!senhaDif", definida após o teste na etapa 1.
             ?> 
             <p class="desc_log">AS SENHAS PRECISAM SER IGUAIS</p><br>
             <?php        
@@ -97,8 +112,8 @@
 
         <input class="btnSeta" type="submit" name="btnSeta" value="➜"><br>
         <a class="cadastro" href="login.html">Já tenho conta</a>
-
-        <input hidden name="txtPagStatus" value="cadastro_1">
+        <!-- O próximo input alterará o $cadastroStatus para cadastro_1 na próxima chamada da página. -->
+        <input hidden name="cadastroStatus" value="cadastro_1"> 
     </form>
 
     <?php
@@ -106,39 +121,51 @@
         
 
         
-        elseif ($pagStatus == 'cadastro_1'){
+        elseif ($cadStatus == 'cadastro_1'){    //# Etapa 1 => caso $cadStatus = "cadastro_1"
 
             $email=$_POST['txtEmail'];
-            $senha=$_POST['txtSenha'];
-            $confirmsenha=$_POST['txtCSenha'];
+            $senha=$_POST['txtSenha'];          
+            $confirmsenha=$_POST['txtCSenha'];  // -> Obtêm os dados a serem cadastrados + senha de confirmação. 
 
-            if($senha == $confirmsenha)
+
+            if($senha != $confirmsenha) //## Caso as senhas sejam diferentes, redireciona o usuário à mesma página com a variável $_GET status = 'erro!senhaDif'.
             {
-                $sql = "INSERT into tbl_usuario"
-                ."(usu_email, usu_senha)"
-                ."VALUES ('$email', '$senha')";
+    ?>
+    <script>
+        window.location.assign("cadastro.php?status=erro!senhaDif");
+    </script>
+    <?php
+            }
+            else //## Executa o código caso a senha normal seja igual a sua confirmação.
+            {
+                $sql = "INSERT into tbl_usuario"    // Criação do comando INSERT para inserção dos dados.
+                    ."(usu_email, usu_senha)"
+                    ."VALUES ('$email', '$senha')";
 
-                getStatusCadastro($conexao, $sql, $pagStatus);
 
-                $sql2 = "SELECT usu_codigo "
+                executarSQL($conexao, $sql, $cadStatus);    // Executa o código SQL em uma função definida no final da página,
+                                                            // o código que mostra os erros está comentado, então não aparecem.
+
+                $sql2 = "SELECT usu_codigo "        // Criação do comando SELECT para obter o código da conta criada.
                     ."FROM tbl_usuario "
                     ."WHERE usu_email = '$email' AND usu_senha = '$senha';";
 
                 $res = mysqli_query($conexao, $sql2);
 
-                if(mysqli_num_rows($res)==0)
+                if(mysqli_num_rows($res)==0)    //### Caso não haja linhas com as condições, expressa-se o erro.
                 {
-                    echo "Usuário não encontrado"; //MUDAR ESSA E OUTRAS MENSAGENS DE ERRO AINDA
+                    echo "Usuário não encontrado"; // *** MUDAR ESSA E OUTRAS MENSAGENS DE ERRO AINDA
                 }
-                else
-                {
+                else                            //### Caso contrário, define a varíavel $codigo como o primeira coluna da consulta
+                {                               // (usu_codigo).
                     $registro = mysqli_fetch_row($res);
                     $codigo = $registro[0];
                 }
     ?>
 
-    <form class="popup" method="POST" action="cadastro.php"> <!-- ## Formulário das informações pessoais -->
-        <div class="tit_log">Crie sua conta</div>
+    <form class="popup" method="POST" action="cadastro.php"> <!-- action define que a página será chamada por si mesma. -->
+
+        <div class="tit_log">Crie sua conta</div> <!-- ## Formulário das informações pessoais -->
         <div class="insc_cad">
             <p class="txt_log">Nome de usuário</p><br>
             <input class="input_cad" type="text" name="txtNome" required placeholder="seu nome de usuário aqui">
@@ -153,50 +180,49 @@
         <input class="btnSeta"  type="submit" name="btnSeta" value="Cadastrar"><br>
         <a class="cadastro" href="login.html">Já tenho conta</a>
 
-        <input hidden name="txtPagStatus" value="cadastro_2">
-        <input hidden name="txtCodigo" value="<?php echo "$codigo"; ?>">
+        <input hidden name="cadastroStatus" value="cadastro_2">
+        <!-- O próximo input alterará o $cadastroStatus para cadastro_2 na próxima chamada da página. -->
+
+        <input hidden name="usuCodigo" value="<?php echo "$codigo"; ?>">
+        <!-- Define uma variável $_POST 'usuCodigo' como o valor do código da nova conta criada armazenado em $codigo. -->
     </form>
 
     <?php
             }
-            else 
-            {
-    ?>
-    <script>
-        window.location.assign("cadastro.php?erro=!senha");
-    </script>
-    <?php
-            }
         }  
 
-        elseif ($pagStatus == "cadastro_2"){
+        elseif ($cadStatus == "cadastro_2"){    //# Etapa 2 => caso $cadStatus = "cadastro_2"
 
-            $codigo = $_POST['txtCodigo'];
+            $codigo = $_POST['usuCodigo'];  // Obtem o código armazenado na variável $_POST.
 
-            $nome = $_POST['txtNome'];
+            $nome = $_POST['txtNome'];  
             $cpf = preg_replace('/[^0-9]/', '', $_POST['txtCpf']);
-            $data = preg_replace('/[^0-9]/', '', $_POST['txtData']);
-            $tel = preg_replace('/[^0-9]/', '', $_POST['txtTelefone']);
+            $data = preg_replace('/[^0-9]/', '', $_POST['txtData']);    // Obtêm as informações a serem atualizadas.
+            $tel = preg_replace('/[^0-9]/', '', $_POST['txtTelefone']); // A função preg_replace substitue caracteres
+                                                                        // não numéricos por nada (remove-os).
 
-            $sql = "UPDATE tbl_usuario "
+            $sql = "UPDATE tbl_usuario "    // Criação do comando UPDATE para atualizar a tabela.
                 ."SET usu_nome = '$nome', usu_cpf = $cpf, usu_nascimento = '20081211', usu_telefone = $tel "
                 ."WHERE usu_codigo = $codigo ;";
 
-            getStatusCadastro($conexao, $sql, $pagStatus)
+            executarSQL($conexao, $sql, $cadStatus);    // Execução do comando SQL.
 
     ?>
     <script>
-         window.location.assign("index.html");
+        // Código javascript para direcionar para a página index.html após execução do código php.
+        window.location.assign("index.html");
     </script>
     <?php
         }
         
 
-        function getStatusCadastro($conexao, $sql, $pagStatus)
+        function executarSQL($conexao, $sql, $cadStatus)    // Definição da função executarSQL()
         {
 
             $res = mysqli_query($conexao, $sql);
 
+
+            // *** MUDAR ESSA E OUTRAS MENSAGENS DE ERRO AINDA
             /*if($res)
             {
             echo "<p align='center'>Usuário cadastrado com sucesso!</p>";
@@ -210,6 +236,6 @@
             }*/
         }
 
-        mysqli_close($conexao);
+        mysqli_close($conexao); // Encerramento da conexão
     ?>
 </body>
