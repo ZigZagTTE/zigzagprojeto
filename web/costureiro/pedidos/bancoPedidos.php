26aceeed750b4d7bf2b38d6146d8009928a62870
heitordemourasardinhatorres <heitor.mourasardinhatorres@gmail.com>
2025-10-14 16:56:52 -0300

    continuação do pedidos

--- web/costureiro/pedidos/bancoPedidos.php
+++ web/costureiro/pedidos/bancoPedidos.php
@@ -1,7 +1,9 @@
 <?php
 require_once('../../conexao.php');
 
-function buscarDadosPedidosResumido($conexao){
 
-    
-};
+$sql = "SELECT tbl_pedido.ped_id FROM tbl_pedido, tbl_item, tbl_catalogo, tbl_costureiro WHERE tbl_pedido.ped_id = tbl_item.ped_id AND tbl_item.cat_id = tbl_catalogo.cat_id AND tbl_catalogo.cos_id = tbl_costureiro.cos_id AND tbl_costureiro.cos_id = $id AND ped_concluido = '0'";
+
+$sqlexecutar = mysqli_query($conexao, $sql);
+
+echo  json_encode(mysqli_fetch_all($sqlexecutar, MYSQLI_ASSOC));
